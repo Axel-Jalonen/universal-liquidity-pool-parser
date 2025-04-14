406d20f9fdfd3bbfa507cbccf8eab166244938a4
Axel Jalonen <axeljalonen@gmail.com>
2025-04-14 11:02:27 -0700

    Add solana_client, RpcClient, and Pubkey re-exports

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,7 @@
 pub mod parsing;
 pub use parsing::get_info_struct;
+
+pub use anchor_client::solana_client;
+pub use anchor_client::solana_client::rpc_client::RpcClient;
+
+pub use anchor_lang::solana_program::pubkey::Pubkey;
